55d29de4af3bd8401ecc3af21dd16364851fde69
Mahiet Bruno <devwebmb@gmail.com>
2021-12-28 14:02:52 +0100

    creation auth.js login et signup

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,37 +1,34 @@
 const bcrypt = require("bcrypt");
-const { User } = require("../models/User");
+const { User } = require("../database/sequelize");
 const jwt = require("jsonwebtoken");
+const privateKey = require("../auth/private_keys");
 
 module.exports = (app) => {
   app.post("/api/auth/login", (req, res) => {
-    User.findAll({
+    User.findOne({
       where: {
         email: req.body.email,
       },
     })
       .then((user) => {
         if (!user) {
-          return res.status(401).json({ error: "Utilisateur non trouvé." });
+          return res.status(404).json({ error: "Utilisateur non trouvé." });
         }
-        bcrypt
-          .compare(req.body.password, user.password)
-          .then((valid) => {
-            if (!valid) {
-              return res.status(401).json({ error: "Mot de passe incorrect." });
-            }
-            res.status(200).json({
-              userId: user._id,
-              token: jwt.sign(
-                {
-                  userId: user._id,
-                },
-                "RANDOM_TOKEN_SECRET",
-                { expiresIn: "24h" }
-              ),
-            });
-          })
-          .catch((error) => res.status(500).json({ error }));
+        bcrypt.compare(req.body.password, user.password).then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect." });
+          }
+          const token = jwt.sign({ userId: user.id }, privateKey, {
+            expiresIn: "24h",
+          });
+          const message = "L'utilisateur a été connecté avec succès";
+          return res.status(200).json({ message, data: user, token });
+        });
       })
-      .catch((error) => res.status(500).json({ error }));
+      .catch((error) => {
+        const message =
+          "L'utilisateur n'a pas pu être connecté, réessayez dans un instant.";
+        return res.json({ message, data: error });
+      });
   });
 };
